f4611248ab06e5788d89b8a06d1975f41b66810d
ringosan1974 <wacyakun@gmail.com>
2022-09-02 21:29:58 +0900

    スロット追加

--- src/cogs.py
+++ src/cogs.py
@@ -1,4 +1,7 @@
+import asyncio
 import disnake
+import random
+
 from disnake.ext import commands
 
 class NoticeComingVC(commands.Cog):
@@ -12,5 +15,28 @@ class NoticeComingVC(commands.Cog):
             channel = member.guild.system_channel
             await channel.send(f'{member.name if member.nick is None else member.nick}さんが{vc}へ入室しました。')
 
+kaito_words = ['２か...ハズレだ', '良い判断だキルア そのままゴンを連れて逃げろ それまでは時間を稼ぐ!!', 'もういい 消えろ']
+
+class CrazySlot(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    def slot(self):
+        result = random.randint(2, 4)
+        return result
+
+    @commands.slash_command()
+    async def crazyslot(self, interaction: disnake.ApplicationCommandInteraction, short=False):
+        word = ''
+        if short is False:
+            word += '口の中にルーレット!! 数字は１から９ 出た目によって武器が変わる!!\nそれがオレ様"気狂いピエロ"さ!!\nいい目が出ろよ!!\n'
+        word += 'ドゥルルルル...'
+        await interaction.response.send_message(word)
+        await asyncio.sleep(2)
+        value = self.slot()
+        word = f'{word}{value}!!\nカイト「{kaito_words[value-2]}」'
+        await interaction.edit_original_message(str(word))
+
 def setup(bot):
     bot.add_cog(NoticeComingVC(bot))
+    bot.add_cog(CrazySlot(bot))
