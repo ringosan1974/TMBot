6c2a4bfdb06f1dc76ddb8d89354419b5903d5ae7
ringosan1974 <wacyakun@gmail.com>
2022-10-15 23:13:34 +0900

    汎用性向上

--- src/msg_img.py
+++ src/msg_img.py
@@ -1,9 +1,11 @@
+from turtle import back
 import disnake
 import os
 
 
 from disnake.ext import commands
 from PIL import Image, ImageDraw, ImageFont
+from var import TRANSPARENT, BLACK
 
 
 BACKGROUND = 'resources/background.jpg'
@@ -13,7 +15,7 @@ SAVEDFILE = 'resources/img.png'
 
 
 class CreateMessageImage(commands.Cog):
-    def init(self, bot):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.slash_command(name='msgimg', description='create image of message')
@@ -21,30 +23,35 @@ class CreateMessageImage(commands.Cog):
         ids = get_element_from_url(url)
         channel = await self.bot.fetch_channel(ids[0])
         message = await channel.fetch_message(ids[1])
-        await save_icon(message)
-        create_msgimg(message.content)
+        await save_icon(os.path.abspath(ICON), message)
+        create_msgimg(BACKGROUND, ICON, message.content)
         await inter.response.send_message(file=disnake.File(SAVEDFILE))
 
 
 # make and save message-image
-def create_msgimg(self, content: str):
-    with Image.open(BACKGROUND).convert('RGBA') as base:
-        with Image.open(ICON).convert('RGBA') as icon:
+def create_msgimg(background: str, avatar: str, content: str):
+    with Image.open(background).convert('RGBA') as base:
+        with Image.open(avatar).convert('RGBA') as iconimage:
             font = ImageFont.truetype(FONT, 80)
-            text = Image.new("RGBA", base.size, (255, 255, 255, 0))
+            text = Image.new("RGBA", base.size, TRANSPARENT)
             drawtext = ImageDraw.Draw(text)
-            drawtext.text((base.width//3, base.height//4), content, font=font, fill=(0, 0, 0, 255))
-            iconbase = Image.new("RGBA", base.size, (255, 255, 255, 0))
-            iconbase.paste(icon, (10, 10))
-            textbase = Image.alpha_composite(base, text)
-            result = Image.alpha_composite(textbase, iconbase)
+            drawtext.text((base.width//3, base.height//4), content, font=font, fill=BLACK)
+            icon = Image.new("RGBA", base.size, TRANSPARENT)
+            icon.paste(iconimage, (10, 10))
+            result = images_alpha_composite(base, icon, text)
             result.save(SAVEDFILE)
 
+def images_alpha_composite(*args):
+    result = Image.new('RGBA', args[0].size, TRANSPARENT)
+    for i in args:
+        result = Image.alpha_composite(result, i)
+    return result
+
 # get channel-id and message-id
-def get_element_from_url(self, url: str):
+def get_element_from_url(url: str):
     pearsed_url = url.split('/')
     return pearsed_url[-2:]  # message-id, channel-id
 
 # save user-icon from message object
-async def save_icon(self, message: disnake.Message):
-    await message.author.avatar.save(os.path.abspath(ICON))
+async def save_icon(fp: os.PathLike, message: disnake.Message):
+    await message.author.avatar.save(fp)
--- src/var.py
+++ src/var.py
@@ -1,8 +1,9 @@
 import os
 
-
 from dotenv import load_dotenv
-
 
 load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
+
+TRANSPARENT = (255, 255, 255, 0)
+BLACK = (0, 0, 0, 255)
