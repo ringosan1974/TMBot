5ae250d90d2b8a19061d28ba083df8c0550dab93
ringosan1974 <wacyakun@gmail.com>
2022-11-06 23:16:37 +0900

    おみくじ追加

--- src/cogs_setup.py
+++ src/cogs_setup.py
@@ -5,9 +5,11 @@ from add_emoji import AddEmoji
 from crazyslot import CrazySlot
 from msg_img import CreateMessageImage
 from notify_vc import NotifyComingVC
+from omikuji import Omikuji
 
 def setup(bot):
     bot.add_cog(CrazySlot(bot))
     bot.add_cog(CreateMessageImage(bot))
     bot.add_cog(NotifyComingVC(bot))
     bot.add_cog(AddEmoji(bot))
+    bot.add_cog(Omikuji(bot))
---
+++ src/omikuji.py
@@ -0,0 +1,19 @@
+import disnake
+import random
+
+
+from disnake.ext import commands
+
+
+class Omikuji(commands.Cog):
+    fortunes = ("大吉", "中吉", "小吉", "凶")
+    lucky_things = ("赤", "青", "水色", "黄色", "緑", "黒", "白", "お姉ちゃんの靴", "靴を舐める", "イヤホン", "女声を出す")
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command()
+    async def omikuji(self, inter):
+        fortune = random.choice(self.fortunes)
+        lucky_thing = random.choice(self.lucky_things)
+        await inter.response.send_message(f"あなたは{fortune}です！\nラッキーなものは{lucky_thing}です！")
