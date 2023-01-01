5c3dcf396b16fe1054d2c8c09d217eedf4d482f7
ringosan1974 <wacyakun@gmail.com>
2023-01-01 21:56:19 +0900

    クソコード改修

--- src/msg_img.py
+++ src/msg_img.py
@@ -1,57 +1,54 @@
-from turtle import back
 import disnake
+import io
 import os
 
 
 from disnake.ext import commands
 from PIL import Image, ImageDraw, ImageFont
-from var import TRANSPARENT, BLACK
-
-
-BACKGROUND = 'resources/background.jpg'
-ICON = 'resources/icon.jpg'
-FONT = 'resources/ShipporiMincho-Bold.ttf'
-SAVEDFILE = 'resources/img.png'
+from var import TRANSPARENT, BLACK, BACKGROUND, FONT
 
 
 class CreateMessageImage(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot, background=BACKGROUND):
         self.bot = bot
+        self.background = background
 
-    @commands.slash_command(name='msgimg', description='create image of message')
+    @commands.slash_command(name='msgimg', description='create image from message')
     async def messageimage(self, inter: disnake.ApplicationCommandInteraction, url: str):
-        ids = get_element_from_url(url)
+        ids = CreateMessageImage.get_ids_from(url)
         channel = await self.bot.fetch_channel(ids[0])
         message = await channel.fetch_message(ids[1])
-        await save_icon(os.path.abspath(ICON), message)
-        create_msgimg(BACKGROUND, ICON, message.content)
-        await inter.response.send_message(file=disnake.File(SAVEDFILE))
 
+        bytesicon = await message.author.avatar.read()
+        icon = Image.open(io.BytesIO(bytesicon))
+        image = self._create_msgimg(icon, message.content)
+        with io.BytesIO() as bytesimage:
+            image.save(bytesimage, format="PNG")
+            bytesimage.seek(0)
+            await inter.response.send_message(file=disnake.File(bytesimage, filename="file.png"))
 
-# make and save message-image
-def create_msgimg(background: str, avatar: str, content: str):
-    with Image.open(background).convert('RGBA') as base:
-        with Image.open(avatar).convert('RGBA') as iconimage:
+    # make and save message-image
+    def _create_msgimg(self, avatar: Image, content: str):
+        with Image.open(self.background).convert('RGBA') as base:
             font = ImageFont.truetype(FONT, 80)
             text = Image.new("RGBA", base.size, TRANSPARENT)
+            icon = Image.new("RGBA", base.size, TRANSPARENT)
+
             drawtext = ImageDraw.Draw(text)
             drawtext.text((base.width//3, base.height//4), content, font=font, fill=BLACK)
-            icon = Image.new("RGBA", base.size, TRANSPARENT)
-            icon.paste(iconimage, (10, 10))
-            result = images_alpha_composite(base, icon, text)
-            result.save(SAVEDFILE)
+            icon.paste(avatar, (10, 10))
+            image = CreateMessageImage.images_alpha_composite(base, icon, text)
+            return image
 
-def images_alpha_composite(*args):
-    result = Image.new('RGBA', args[0].size, TRANSPARENT)
-    for i in args:
-        result = Image.alpha_composite(result, i)
-    return result
+    # get channel-id and message-id
+    @staticmethod
+    def get_ids_from(url: str):
+        pearsed_url = url.split('/')
+        return pearsed_url[-2:]  # message-id, channel-id
 
-# get channel-id and message-id
-def get_element_from_url(url: str):
-    pearsed_url = url.split('/')
-    return pearsed_url[-2:]  # message-id, channel-id
-
-# save user-icon from message object
-async def save_icon(fp: os.PathLike, message: disnake.Message):
-    await message.author.avatar.save(fp)
+    @staticmethod
+    def images_alpha_composite(*args):
+        result = Image.new('RGBA', args[0].size, TRANSPARENT)
+        for i in args:
+            result = Image.alpha_composite(result, i)
+        return result
--- src/var.py
+++ src/var.py
@@ -7,3 +7,5 @@ TOKEN = os.getenv('BOT_TOKEN')
 
 TRANSPARENT = (255, 255, 255, 0)
 BLACK = (0, 0, 0, 255)
+BACKGROUND = 'resources/background.jpg'
+FONT = 'resources/ShipporiMincho-Bold.ttf'
